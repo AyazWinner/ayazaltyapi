1ed56b1fa0695f641156a92a286380eb97136fc1
AyazWinner <56387944+AyazWinner@users.noreply.github.com>
2019-10-31 12:12:56 +0300

    Update resimlimesaj.js

--- komutlar/resimlimesaj.js
+++ komutlar/resimlimesaj.js
@@ -1,6 +1,6 @@
 client.on('message', message => {
     if (message.content === 'sa') {
-        const attachment = new Attachment('https://pbs.twimg.com/media/DzHYmeHWsAA8QXW.jpg');
+        const attachment = new Attachment('https://media.giphy.com/media/XDMRnqsUjno0L88kja/giphy.gif');
         message.channel.send(attachment);
     }
 });
