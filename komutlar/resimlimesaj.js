5557a922b26341da783d96bcfdf78e94c2ef3070
AyazWinner <56387944+AyazWinner@users.noreply.github.com>
2019-10-31 21:34:04 +0300

    Delete resimlimesaj.js

--- komutlar/resimlimesaj.js
+++
@@ -1,6 +0,0 @@
-client.on('message', message => {
-    if (message.content === 'sa') {
-        const attachment = new Attachment('https://media.giphy.com/media/XDMRnqsUjno0L88kja/giphy.gif');
-        message.channel.send(attachment);
-    }
-});
